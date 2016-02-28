3a43dc558ea4dcf569a8e0c919838d878a6e2c4e
Lawrence Agbani <L.agbani@yahoo.com>
2016-02-28 15:04:41 +0100

    Fixing not found error

--- application/modules/Admin/controllers/Dashboard.php
+++ application/modules/Admin/controllers/Dashboard.php
@@ -4,7 +4,7 @@ use Parse\ParseObject;
 use Parse\ParseUser;
 use Parse\ParseQuery;
 use Parse\ParseRole;
-include trim(dirname(BASEPATH).PHP_EOL.'/vendor/phpoffice/Classes/PHPExcel/Reader/IReadFilter.php', '');
+include trim(dirname(BASEPATH).'vendor/phpoffice/Classes/PHPExcel/Reader/IReadFilter.php', '');
 
 class Dashboard extends CI_Controller {
 
