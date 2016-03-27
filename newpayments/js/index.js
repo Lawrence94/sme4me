a8c2d575d27739246a3cd6fdb8ddabd639832e7b
joshuajosh59 <joshuajosh59@gmail.com>
2016-03-27 18:58:15 +0100

    new card

--- newpayments/js/index.js
+++ newpayments/js/index.js
@@ -4,3 +4,16 @@
     $(this).toggleClass('active');
   });
 }); */
+$(function() {
+  $('.pop-up').hide();
+  $('.pop-up').fadeIn(1000);
+  
+      $('.close-button').click(function (e) { 
+
+      $('.pop-up').fadeOut(700);
+      $('#overlay').removeClass('blur-in');
+      $('#overlay').addClass('blur-out');
+      e.stopPropagation();
+        
+    });
+ });
